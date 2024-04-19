66f8b16380803c331ce7900e446a92aeafa5b129
ehermandeveloper <ehermandeveloper@gmail.com>
2024-04-19 08:26:47 +0530

    add_apikey

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const ytdt = require("ytdl-core");
+var cors = require("cors");
+const portNumber = 5000;
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.get("/download", async (req, res) => {
+  try {
+    const { api_key, url } = req.query;
+    if (api_key != process.env.APIKEY) {
+      return res
+        .status(403)
+        .send({ message: `Your APIKEY is incorrect: ${api_key}` });
+    } else {
+      const videoId = await ytdt.getURLVideoID(url);
+      const metaInfo = await ytdt.getInfo(url);
+      let data = {
+        url: "https://www.youtube.com/embed/" + videoId,
+        info: metaInfo.formats,
+      };
+      return res.status(200).send(data);
+    }
+  } catch (error) {
+    return console.log(error);
+  }
+});
+app.listen(portNumber, () =>
+  console.log(`Example app is listening on port ${portNumber}.`)
+);
