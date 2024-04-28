65d2f1d5c02913973f04b8e0e3f282e082770343
ehermandeveloper <ehermandeveloper@gmail.com>
2024-04-28 12:10:49 +0530

    remove_unwantedcode

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// const express = require("express");
-// const ytdt = require("ytdl-core");
-// var cors = require("cors");
-// const portNumber = 5000;
-// // require("dotenv").config();
-
-// const app = express();
-// app.use(cors());
-// app.get("/download", async (req, res) => {
-//   try {
-//     const {  url } = req.query;
-//     const videoId = await ytdt.getURLVideoID(url);
-//       const metaInfo = await ytdt.getInfo(url);
-//       let data = {
-//         url: "https://www.youtube.com/embed/" + videoId,
-//         info: metaInfo.formats,
-//       };
-//       return res.status(200).send(data);
-//   } catch (error) {
-//     return console.log(error);
-//   }
-// });
-// app.listen(portNumber, () =>
-//   console.log(`Example app is listening on port ${portNumber}.`)
-// );
-
 const express = require("express");
 const ytdt = require("ytdl-core");
 const cors = require("cors");
